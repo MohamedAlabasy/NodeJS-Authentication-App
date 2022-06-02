00f6bee8f2d330042d39e778d96060792647713a
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-06-02 20:02:05 +0200

    add bcryptjs lib

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,4 +1,6 @@
-const auth = require('../Models/AuthSchema');
+const bcrypt = require('bcryptjs');
+
+const Auth = require('../Models/AuthSchema');
 
 exports.login = (request, response, next) => {
     response.status(200).json({
@@ -8,13 +10,26 @@ exports.login = (request, response, next) => {
 }
 
 exports.register = (request, response, next) => {
-    response.status(200).json({
-        status: 1,
-        data: 'register',
+    let hash = bcrypt.hashSync(request.body.password, 10);
+    let user = new Auth({
+        name: request.body.name,
+        email: request.body.email,
+        gender: request.body.gender,
+        password: hash,
     })
+    user.save()
+        .then((data) => {
+            response.status(200).json({
+                status: 1,
+                data: data,
+            })
+        })
+        .catch((error) => {
+            next(error)
+        })
 }
 exports.getUserData = (request, response, next) => {
-    auth.findById(request.body.id);
+    Auth.findById(request.body.id);
     response.status(200).json({
         status: 1,
         data: 'get User Data',
@@ -22,10 +37,10 @@ exports.getUserData = (request, response, next) => {
 }
 
 exports.getAllUsersData = (request, response, next) => {
-    auth.find({})
+    Auth.find({})
         .then(data => {
             if (data == null) {
-                throw new Error('No user to show');
+                throw new Error('No user to show')
             } else {
                 response.status(200).json({
                     status: 1,
--- Models/AuthSchema.js
+++ Models/AuthSchema.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const auto_increment = require('@typegoose/auto-increment');
+const { AutoIncrementID } = require('@typegoose/auto-increment');
 
 // 1- build schema with validation
 const schema = new mongoose.Schema({
@@ -10,7 +10,6 @@ const schema = new mongoose.Schema({
     gender: { type: String, enum: ['male', 'female'], default: 'male' },
 }, { timestamps: true });
 
-schema.plugin(auto_increment, [{ field: '_id' }])
-
+schema.plugin(AutoIncrementID, [{ field: '_id' }]);
 // Register for schema in mongoose
 module.exports = mongoose.model('users', schema);
