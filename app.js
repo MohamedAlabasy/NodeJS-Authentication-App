a02747da3801ded6d8af8da105c2436b5261826e
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-06-04 22:34:19 +0200

    finish task

--- app.js
+++ app.js
@@ -2,20 +2,23 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const body_parser = require('body-parser');
+
 const router = require('./Routers/AuthRouter');
 
 require('dotenv').config();
 const app = express();
-
+// #=======================================================================================#
+// #			                        connect mongoose                                   #
+// #=======================================================================================#
 mongoose.connect(process.env.MONGO_DB)
     .then((data) => {
-        console.log('DB connected ... ');
+        console.log('DB connected ...');
         // run server
         app.listen(process.env.PORT || PORT, () => {
             console.log(`App Run at http://${process.env.HOST}:${process.env.PORT || 8050}`);
         });
     }).catch((error) => {
-        console.log('DB not connected', error + '');
+        console.log('DB not connected : ' + error);
     })
 
 // // middleware to get method and url
@@ -24,8 +27,11 @@ mongoose.connect(process.env.MONGO_DB)
 //     next();
 // })
 app.use(morgan('tiny'));
-//to add header or use cors
 
+
+// #=======================================================================================#
+// #			                     add header or use cors                                #
+// #=======================================================================================#
 app.use((request, response, next) => {
     response.header("Access-Control-Allow-Origin", "*");//alow to any web side to connect to my server
     response.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS"); //for routs
@@ -33,12 +39,22 @@ app.use((request, response, next) => {
     next();
 });
 
+
+// #=======================================================================================#
+// #			                            body_parse                                     #
+// #=======================================================================================#
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({ extended: false }));
-// app.use(checkTokens);
+
+
+// #=======================================================================================#
+// #			                            router                                         #
+// #=======================================================================================#
 app.use('', router);
 
-// middleware not Found
+// #=======================================================================================#
+// #			                        not Found middleware                               #
+// #=======================================================================================#
 app.use((request, response, next) => {
     response.status(404).json({
         status: 0,
@@ -46,7 +62,9 @@ app.use((request, response, next) => {
     })
 })
 
-// middleware error
+// #=======================================================================================#
+// #			                      error middleware                                     #
+// #=======================================================================================#
 app.use((error, request, response, next) => {
     let status = error.status || 500;
     response.status(status).json({
